2770c74d1c0f9b5c0cc43dd25ba17da636e927d7
Leticia Rostirola <lovelace@Hadouken.local>
2013-07-27 16:23:53 -0300

    First

---
+++ mongo.py
@@ -0,0 +1,34 @@
+from pymongo import MongoClient
+from pymongo import Connection
+from pymongo.errors import ConnectionFailure
+
+def main():
+#Connect to MongoDB
+	try:
+		c = Connection(host="localhost", port=27017)
+		print ("Connected sucessfully")
+	except ConnectionFailure, e:
+		sys.stderr.write("Could not connect to MongoDB: %s" % e)
+		sys.exit(1)
+	# Get a Database handle to a database named "mydb -> juntadb"
+	dbh = c["juntadb"]
+	# Demonstrate the db.connection property to retrieve a reference to the
+	# Connection object should it go out of scope. In most cases, keeping a
+	# reference to the Database object for the lifetime of your program should
+	# be sufficient.
+	assert dbh.connection == c
+	print "Successfully set up a database handle"
+	#Busca Todas as empresas
+	companies = dbh.Data.find()
+	#Imprime todas as empresas
+	for company in companies:
+		print (company)
+	# Contar quantas empresas existem cadastradas
+	countCompanies = dbh.Data.find().count()
+	print "There are %d documents in Data collection" % countCompanies
+
+	searchComp = raw_input("Empresa: ")
+	company = dbh.Data.find_one({"name": searchComp})
+	print (company)
+if __name__ == "__main__":
+		main()
