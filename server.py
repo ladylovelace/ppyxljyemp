b1735da2a6f67a191682440a83436ba774a22fe6
Jean <jeanlucas@usp.br>
2013-07-31 20:29:24 -0300

    Server up

--- server.py
+++ server.py
@@ -2,10 +2,19 @@ import http.server
 import socketserver
 import urllib.parse
 
-class Handler(http.server.SimpleHTTPRequestHandler):
+class httpHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.log_message("%s %s" % (self.command, self.path) )
         self.dumpReq( None )
+
+    def do_POST(self):
+        self.log_message("%s %s" % (self.command, self.path) )
+        if self.headers.has_key('content-length'):
+            length= int( self.headers['content-length'] )
+            self.dumpReq( self.rfile.read( length ) )
+        else:
+            self.dumpReq( None )
+
     def dumpReq( self, formInput=None ):   
         response= "<html><head></head><body>"
         response+= "<p>boo</p>"
@@ -14,6 +23,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         response+= "<p>self.path= <tt>%s</tt></p>" % ( self.path )
         response+= "</body></html>"
         self.sendPage( "text/html", response )
+
     def sendPage( self, type, body ):
         self.send_response( 200 )
         self.send_header( "Content-type", type )
@@ -23,7 +33,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
 
 def main():
     try:
-        server = socketserver.TCPServer(("", 80), Handler)
+        server = socketserver.TCPServer(("", 80), httpHandler)
         print ("AWWW YEAHHH!1")
         server.serve_forever()
     except KeyboardInterrupt:
